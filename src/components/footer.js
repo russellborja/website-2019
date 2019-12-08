acbf0c16affdfe05a34881bc3faec6de4b5694a6
russellborja <russellborja@gmail.com>
2019-12-08 16:15:07 -0500

    Add scroll to top from bottom of page

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,14 +2,31 @@ import React from "react"
 import styles from "../styles/footer.module.css"
 import Container from "react-bootstrap/Container"
 import Contact from "./contact"
+import { Link, animateScroll as scroll } from "react-scroll";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
 
-const Footer = () => (
-    <div className={styles.footer}>
-        <Container>
-            <Contact/>
-            <p>© Copyright 2019 Russell Borja. Built from scratch using Gatsby and React-Bootstrap.</p>
-        </Container>
-    </div>
-)
+export default class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-export default Footer
+    scrollToTop = () => {
+        scroll.scrollToTop();
+    }
+
+    render() {
+        return (
+            <div className={styles.footer}>
+                <Container>
+                
+                    <Link onClick={this.scrollToTop} className={styles.scrolldown} duration={500} spy={true} offset={10} smooth={true}><FontAwesomeIcon icon={faAngleUp}/></Link>
+                
+                
+                <Contact/>
+                <p>© Copyright 2019 Russell Borja. Built from scratch using Gatsby and React-Bootstrap.</p>
+                </Container>
+            </div>
+        )
+    }
+}
