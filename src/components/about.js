5c5859ccd29378a542f7f117b009dd33bf668cc9
russellborja <russellborja@gmail.com>
2019-12-09 20:13:37 -0500

    Make profile pic higher quality

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styles from "../styles/about.module.css"
-import profilePic from "../images/profile.jpg"
+import profilePic from "../images/profile.png"
 import Container from "react-bootstrap/Container"
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
